2ed0abe9b8e52a7735c87a02ed6f5a44f996e419
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-03-07 09:32:52 +0800

    chore(sem): 2020-2021 sem2

--- src/config.js
+++ src/config.js
@@ -1,16 +1,11 @@
 export const SEMESTER={
-    name: '2019-2020 第二学期',
-    id: '2019-2020 sem2', // checked before importing
-    begin_date: new Date(2020,2-1,17, 0,0,0,0),
+    name: '2020-2021 第二学期',
+    id: '2020-2021 sem2', // checked before importing
+    begin_date: new Date(2021,3-1,8, 0,0,0,0),
     weeks: 16,
     exclude_dates: [
-        new Date(2020,4-1,24, 0,0,0,0), // 运动会
-        new Date(2020,5-1,1, 0,0,0,0), // 五一
-        new Date(2020,5-1,4, 0,0,0,0),
-        new Date(2020,5-1,5, 0,0,0,0),
-        new Date(2020,5-1,6, 0,0,0,0),
-        new Date(2020,5-1,7, 0,0,0,0),
-        new Date(2020,5-1,8, 0,0,0,0),
+        new Date(2021,4-1,30, 0,0,0,0), // 五一
+        new Date(2021,5-1,1, 0,0,0,0), // 五一
     ]
 };
 
