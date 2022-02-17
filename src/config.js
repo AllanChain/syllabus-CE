a32ce9ad39c083a5ea1d157e617afa3a52ebdaa2
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-02-17 15:37:50 +0800

    2021-2022 sem2

--- src/config.js
+++ src/config.js
@@ -1,17 +1,15 @@
 export const SEMESTER={
-    name: '2021-2022 第一学期',
-    id: '2021-2022 sem1', // checked before importing
-    begin_date: new Date(2021,9-1,13, 0,0,0,0),
+    name: '2021-2022 第二学期',
+    id: '2021-2022 sem2', // checked before importing
+    begin_date: new Date(2021,2-1,21, 0,0,0,0),
     weeks: 16,
     exclude_dates: [
-        new Date(2021,9-1,21, 0,0,0,0), // 中秋
-        new Date(2021,10-1,1, 0,0,0,0), // 国庆
-        new Date(2021,10-1,2, 0,0,0,0),
-        new Date(2021,10-1,3, 0,0,0,0),
-        new Date(2021,10-1,4, 0,0,0,0),
-        new Date(2021,10-1,5, 0,0,0,0),
-        new Date(2021,10-1,6, 0,0,0,0),
-        new Date(2021,10-1,7, 0,0,0,0),
+        new Date(2021,5-1,1, 0,0,0,0), // 五一
+        new Date(2021,5-1,2, 0,0,0,0),
+        new Date(2021,5-1,3, 0,0,0,0),
+        new Date(2021,5-1,4, 0,0,0,0),
+        new Date(2021,5-1,5, 0,0,0,0),
+        new Date(2021,5-1,6, 0,0,0,0),
     ]
 };
 
