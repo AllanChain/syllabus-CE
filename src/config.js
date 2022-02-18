3a13a5861df3df4bb379bfd0b298357bd4a26d5d
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-02-18 22:23:51 +0800

    fix year
    Fix #1

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 export const SEMESTER={
     name: '2021-2022 第二学期',
     id: '2021-2022 sem2', // checked before importing
-    begin_date: new Date(2021,2-1,21, 0,0,0,0),
+    begin_date: new Date(2022,2-1,21, 0,0,0,0),
     weeks: 16,
     exclude_dates: [
-        new Date(2021,5-1,1, 0,0,0,0), // 五一
-        new Date(2021,5-1,2, 0,0,0,0),
-        new Date(2021,5-1,3, 0,0,0,0),
-        new Date(2021,5-1,4, 0,0,0,0),
-        new Date(2021,5-1,5, 0,0,0,0),
-        new Date(2021,5-1,6, 0,0,0,0),
+        new Date(2022,5-1,1, 0,0,0,0), // 五一
+        new Date(2022,5-1,2, 0,0,0,0),
+        new Date(2022,5-1,3, 0,0,0,0),
+        new Date(2022,5-1,4, 0,0,0,0),
+        new Date(2022,5-1,5, 0,0,0,0),
+        new Date(2022,5-1,6, 0,0,0,0),
     ]
 };
 
